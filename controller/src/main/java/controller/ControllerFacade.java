7c8e37b6eeef56434a51381861a7e08281c089bc
YannisTaibi <33619058+YannisTaibi@users.noreply.github.com>
2018-06-01 15:07:38 +0200

    .

--- controller/src/main/java/controller/ControllerFacade.java
+++ controller/src/main/java/controller/ControllerFacade.java
@@ -10,8 +10,6 @@ import view.IView;
 /**
  * <h1>The Class ControllerFacade provides a facade of the Controller component.</h1>
  *
- * @author Jean-Aymeric DIET jadiet@cesi.fr
- * @version 1.0
  */
 public class ControllerFacade implements IController {
 
