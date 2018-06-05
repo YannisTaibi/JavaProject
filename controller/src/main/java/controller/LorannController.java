71138d232a82de65305e45e1ad3708821d238f6b
BaneOfSins <aart.anema@viacesi.fr>
2018-06-05 10:37:24 +0200

    pom

--- controller/src/main/java/controller/LorannController.java
+++ controller/src/main/java/controller/LorannController.java
@@ -1,65 +1,25 @@
 package controller;
 
-import view.*;
+import model.ILorannModel;
 
 public class LorannController implements IOrderPerformer, ILorannController {
 
-	private int TIME_SLEEP = 100;
+	private final int TIME_SLEEP = 100;
+	private ILorannModel model;
 
 	/**
-	 * 
+	 *
 	 * @param lorannModel
 	 * @param lorannView
 	 */
 	public LorannController(ILorannModel lorannModel, ILorannView lorannView) {
-		// TODO - implement LorannController.LorannController
-		throw new UnsupportedOperationException();
-	}
-
-	public void play() {
-		// TODO - implement LorannController.play
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 * 
-	 * @param userOrder
-	 */
-	public void orderPerform(UserOrder userOrder) {
-		// TODO - implement LorannController.orderPerform
-		throw new UnsupportedOperationException();
-	}
-
-	public IOrderPerformer getOrderPerformer() {
-		// TODO - implement LorannController.getOrderPerformer
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 * 
-	 * @param viewSystem
-	 */
-	public void setViewSystem(IViewSystem viewSystem) {
-		// TODO - implement LorannController.setViewSystem
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 * 
-	 * @param player
-	 */
-	private void castSpell(int player) {
-		// TODO - implement LorannController.castSpell
-		throw new UnsupportedOperationException();
+		this.setView(view);
+		this.setModel(this.model);
+		this.clearStackOrder();
 	}
 
 	private void gameLoop() {
 		// TODO - implement LorannController.gameLoop
-		throw new UnsupportedOperationException();
-	}
-
-	private ILorannView getView() {
-		// TODO - implement LorannController.getView
 		throw new UnsupportedOperationException();
 	}
 
@@ -68,8 +28,41 @@ public class LorannController implements IOrderPerformer, ILorannController {
 		throw new UnsupportedOperationException();
 	}
 
+	@Override
+	public IOrderPerformer getOrderPerformer() {
+		return this;
+	}
+
+	private ILorannView getView() {
+		// TODO - implement LorannController.getView
+		throw new UnsupportedOperationException();
+	}
+
 	/**
-	 * 
+	 *
+	 * @param userOrder
+	 */
+	@Override
+	public void orderPerform(UserOrder userOrder) {
+		this.setStackOrder(userOrder);
+	}
+
+	@Override
+	public void play() {
+		// TODO - implement LorannController.play
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 *
+	 * @param model
+	 */
+	private void setModel(ILorannModel model) {
+		this.model = model;
+	}
+
+	/**
+	 *
 	 * @param view
 	 */
 	private view setView(ILorannView view) {
@@ -78,12 +71,11 @@ public class LorannController implements IOrderPerformer, ILorannController {
 	}
 
 	/**
-	 * 
-	 * @param model
+	 *
+	 * @param viewSystem
 	 */
-	private void setModel(ILorannModel model) {
-		// TODO - implement LorannController.setModel
-		throw new UnsupportedOperationException();
+	public void setViewSystem(IViewSystem viewSystem) {
+		this.viewSystem = viewSystem;
 	}
 
 }
